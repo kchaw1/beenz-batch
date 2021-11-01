211a784ec024d6e665188e8c6ff30d2c9fc968c1
kchaw1 <heatarin@gmail.com>
2021-11-01 17:44:15 +0900

    조건에 따른 스텝 처리, 분기처리

--- src/main/java/com/example/beenz/batch/job/example/SimpleJobConfiguration.java
+++ src/main/java/com/example/beenz/batch/job/example/SimpleJobConfiguration.java
@@ -1,34 +1,38 @@
-package com.example.beenz.batch.job;
+package com.example.beenz.batch.job.example;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j // log 사용을 위한 lombok 어노테이션
-@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class SimpleJobConfiguration {
-    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
-    private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
